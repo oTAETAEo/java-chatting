625ac899a223a88f363170036b24a34f75614817
oTAETAEo <ggg7515@naver.com>
2025-11-05 00:33:57 +0900

    setting commit

---
+++ src/main/java/woowacourse/chatting/ChattingApplication.java
@@ -0,0 +1,13 @@
+package woowacourse.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingApplication.class, args);
+	}
+
+}
