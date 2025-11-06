5ff87116bfa4152119b7c8f37d7511b029bcca3d
oTAETAEo <ggg7515@naver.com>
2025-11-06 21:12:02 +0900

    feat(SignInDto) : 로그인 정보 Dto 추가

---
+++ src/main/java/woowacourse/chatting/dto/SignInDto.java
@@ -0,0 +1,15 @@
+package woowacourse.chatting.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SignInDto {
+    private String email;
+    private String password;
+}
