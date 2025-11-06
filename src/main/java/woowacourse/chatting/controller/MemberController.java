71d606cf88cb88438a6172ae70761a02b0368c28
oTAETAEo <ggg7515@naver.com>
2025-11-06 21:10:09 +0900

    feat(MemberController) : 로그인 기능 추가

--- src/main/java/woowacourse/chatting/controller/MemberController.java
+++ src/main/java/woowacourse/chatting/controller/MemberController.java
@@ -1,24 +1,59 @@
 package woowacourse.chatting.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import woowacourse.chatting.dto.AddMemberRequest;
 import woowacourse.chatting.dto.ResponseDto;
-import woowacourse.chatting.service.MemberService;
+import woowacourse.chatting.dto.SignInDto;
+import woowacourse.chatting.jwt.JwtToken;
+import woowacourse.chatting.service.MemberServiceImp;
 
+import java.net.http.HttpResponse;
+import java.util.List;
+
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class MemberController {
 
-    private final MemberService memberService;
+    private final MemberServiceImp memberServiceImp;
 
-    @PostMapping("/signUp")
-    public ResponseEntity<ResponseDto> memberSignUp(@Validated @RequestBody AddMemberRequest dto){
-        memberService.save(dto);
+    @PostMapping("/sign-up")
+    public ResponseEntity<ResponseDto> memberSignUp(@Validated @RequestBody AddMemberRequest dto) {
+        memberServiceImp.save(dto);
         return ResponseEntity.ok(new ResponseDto("회원가입이 완료되었습니다"));
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> singIn(@RequestBody SignInDto signInDto, HttpServletResponse response) {
+        JwtToken jwtToken = memberServiceImp.singIn(signInDto.getEmail(), signInDto.getPassword());
+
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", jwtToken.getRefreshToken().getToken())
+                .httpOnly(true)       // JS 접근 불가
+                .secure(false)         // HTTP 환경에서도 전송,
+                .sameSite("Lax")      // CSRF 방지
+                .path("/auth/refresh") // 재발급 엔드포인트에만 전송되도록 경로 설정
+                .maxAge(86400 * 30)   // 쿠키 만료 시간 (예: 30일)
+                .build();
+
+        // 3. 응답 헤더에 쿠키 추가
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+
+        return ResponseEntity.ok(jwtToken.getAccessToken());
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "success";
     }
 }
