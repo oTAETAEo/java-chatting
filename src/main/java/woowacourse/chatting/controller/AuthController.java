8cc0044b62545dafffd912b578efe94ea1624149
oTAETAEo <ggg7515@naver.com>
2025-11-07 21:22:01 +0900

    feat(AuthController) : 로그인 관련 엔드포인트 분리

---
+++ src/main/java/woowacourse/chatting/controller/AuthController.java
@@ -0,0 +1,55 @@
+package woowacourse.chatting.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import woowacourse.chatting.dto.AddMemberRequest;
+import woowacourse.chatting.dto.ResponseDto;
+import woowacourse.chatting.dto.ResponseToken;
+import woowacourse.chatting.dto.SignInDto;
+import woowacourse.chatting.jwt.JwtToken;
+import woowacourse.chatting.service.AuthService;
+import woowacourse.chatting.service.MemberServiceImp;
+
+@RestController
+@AllArgsConstructor
+public class AuthController {
+
+    private final MemberServiceImp memberServiceImp;
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<ResponseDto> memberSignUp(@Validated @RequestBody AddMemberRequest dto) {
+        memberServiceImp.save(dto);
+        return ResponseEntity.ok(new ResponseDto("회원가입이 완료되었습니다"));
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<ResponseToken> singIn(@RequestBody SignInDto signInDto, HttpServletResponse response) {
+        JwtToken jwtToken = authService.singIn(signInDto);
+
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", jwtToken.getRefreshToken())
+                .httpOnly(true)       // JS 접근 불가
+                .secure(false)         // HTTP 환경에서도 전송,
+                .sameSite("Lax")      // CSRF 방지
+                .path("/jwt/refresh")
+                .maxAge(86400 * 30)   // 쿠키 만료 시간 (예: 30일)
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+
+        return ResponseEntity.ok(
+                ResponseToken.builder()
+                        .grantType(jwtToken.getGrantType())
+                        .accessToken(jwtToken.getAccessToken())
+                        .build()
+        );
+    }
+}
