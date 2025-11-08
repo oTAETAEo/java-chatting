a97c1ed7feae3c9cea199b4caea108fe53b2931b
oTAETAEo <ggg7515@naver.com>
2025-11-08 15:14:44 +0900

    feat(RefreshTokeRepository) : MemberId를 사용한 find 기능 추가

--- src/main/java/woowacourse/chatting/repository/RefreshTokeRepository.java
+++ src/main/java/woowacourse/chatting/repository/RefreshTokeRepository.java
@@ -2,9 +2,18 @@ package woowacourse.chatting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import woowacourse.chatting.domain.RefreshToken;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RefreshTokeRepository extends JpaRepository<RefreshToken, Long> {
 
+    @Transactional
+    void deleteByMemberId(Long memberId);
+
+    @Transactional
+    Optional<RefreshToken> findByMemberId(Long memberId);
 }
