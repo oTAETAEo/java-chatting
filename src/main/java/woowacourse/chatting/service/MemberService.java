3c0ee7521969f73534b0d34e334355b77911f09c
oTAETAEo <ggg7515@naver.com>
2025-11-10 19:57:41 +0900

    refactor(MemberService) : 인터페이스 save() 메서드 추가

--- src/main/java/woowacourse/chatting/service/MemberService.java
+++ src/main/java/woowacourse/chatting/service/MemberService.java
@@ -1,10 +1,11 @@
 package woowacourse.chatting.service;
 
 import woowacourse.chatting.domain.Member;
+import woowacourse.chatting.dto.AddMemberRequest;
 
 
 public interface MemberService {
 
     Member findMember(Long memberId);
-
+    Long save(AddMemberRequest dto);
 }
