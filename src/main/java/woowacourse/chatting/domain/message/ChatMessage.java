044081de1d74d276a1526fa07d1c43b790da9a12
oTAETAEo <ggg7515@naver.com>
2025-11-10 20:03:01 +0900

    feat(ChatMessage) : 메시지 저장 및 채팅방 아이디 저장 객체

---
+++ src/main/java/woowacourse/chatting/domain/message/ChatMessage.java
@@ -0,0 +1,28 @@
+package woowacourse.chatting.domain.message;
+
+import jakarta.persistence.*;
+import lombok.*;
+import woowacourse.chatting.domain.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChatMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_message_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;       // 어떤 채팅방의 메시지인지
+
+    private String sender;       // 보낸 사람
+    private String content;      // 메시지 본문
+
+    @Enumerated(EnumType.STRING)
+    private MessageType type;
+}
---
+++ src/main/java/woowacourse/chatting/repository/message/ChatMessageRepository.java
@@ -0,0 +1,15 @@
+package woowacourse.chatting.repository.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import woowacourse.chatting.domain.message.ChatMessage;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    @Query("select m from ChatMessage m where m.chatRoom.id = :roomId order by m.createdAt asc")
+    List<ChatMessage> findHistoryMessage(@Param("roomId") UUID chatRoomId);
+}
