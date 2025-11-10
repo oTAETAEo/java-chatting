4e47ce50a98b87511d238a3a1d73f857279b867e
oTAETAEo <ggg7515@naver.com>
2025-11-10 09:18:55 +0900

    feat(ChatMessage) : 메시지 구조 추가

---
+++ src/main/java/woowacourse/chatting/dto/ChatMessage.java
@@ -0,0 +1,14 @@
+package woowacourse.chatting.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatMessage{
+
+    private String sender;
+    private String content;
+}
