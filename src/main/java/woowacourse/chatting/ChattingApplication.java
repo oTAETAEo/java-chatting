5ba36e35c3a5c04e46ef2380536f2b2933446c18
oTAETAEo <ggg7515@naver.com>
2025-11-10 19:58:54 +0900

    feat(ChattingApplication) : 테스트 Member 데이터 세팅

--- src/main/java/woowacourse/chatting/ChattingApplication.java
+++ src/main/java/woowacourse/chatting/ChattingApplication.java
@@ -1,7 +1,17 @@
 package woowacourse.chatting;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import woowacourse.chatting.domain.message.ChatRoom;
+import woowacourse.chatting.dto.AddMemberRequest;
+import woowacourse.chatting.repository.message.ChatRoomRepository;
+import woowacourse.chatting.service.MemberService;
+
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class ChattingApplication {
@@ -10,4 +20,20 @@ public class ChattingApplication {
         SpringApplication.run(ChattingApplication.class, args);
     }
 
+    @Autowired
+    MemberService memberService;
+
+    @PostConstruct
+    public void init(){
+        memberService.save(new AddMemberRequest(
+                "test@test.com"
+                ,"홍길동"
+                ,"1111")
+        );
+        memberService.save(new AddMemberRequest(
+                "1@1"
+                ,"강감찬"
+                ,"1111")
+        );
+    }
 }
