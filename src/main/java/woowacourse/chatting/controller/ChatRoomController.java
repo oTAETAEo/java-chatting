2fccd74d6f9c41ade63d8acb399368b651a87681
oTAETAEo <ggg7515@naver.com>
2025-11-11 20:58:44 +0900

    feat(ChatRoomController) : 개인 채팅방 관련 컨트롤러
    - [추가] 1대1 채팅방의 ChatRoom ID를 알아오기 위한 API 작성

---
+++ src/main/java/woowacourse/chatting/controller/ChatRoomController.java
@@ -0,0 +1,49 @@
+package woowacourse.chatting.controller;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import woowacourse.chatting.domain.Member;
+import woowacourse.chatting.domain.message.ChatRoom;
+import woowacourse.chatting.domain.message.ChatRoomType;
+import woowacourse.chatting.dto.message.PrivateRoomRequest;
+import woowacourse.chatting.dto.message.PrivateRoomResponse;
+import woowacourse.chatting.repository.message.ChatRoomRepository;
+import woowacourse.chatting.service.MemberService;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberService memberService;
+
+    @PostMapping("/api/chat/private-room")
+    public ResponseEntity<?> getPrivateChatRoomId(@RequestBody PrivateRoomRequest roomRequest, @AuthenticationPrincipal Member member){
+        Optional<ChatRoom> findChatRoom = chatRoomRepository.findByUsers(member.getEmail(), roomRequest.getRecipientUsername(), ChatRoomType.PRIVATE);
+
+        if (findChatRoom.isPresent()){
+            return ResponseEntity.ok(new PrivateRoomResponse(findChatRoom.get().getId()));
+        }
+
+        Member recipientMember = memberService.findByEmailMember(roomRequest.getRecipientUsername());
+        Member senderMember = memberService.findMember(member.getId());
+        ChatRoom createChatRoom = new ChatRoom(UUID.randomUUID(), Set.of(recipientMember, senderMember), ChatRoomType.PRIVATE);
+        chatRoomRepository.save(createChatRoom);
+
+        return ResponseEntity.ok(new PrivateRoomResponse(createChatRoom.getId()));
+    }
+}
+
