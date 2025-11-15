de9a049b3e17361b6666fc407e75c5954ba57855
oTAETAEo <ggg7515@naver.com>
2025-11-15 21:54:59 +0900

    refactor(FriendRelation) : 상태 변경을 위한 매서드 추가 및 지연 로딩 적용

--- src/main/java/woowacourse/chatting/domain/member/FriendRelation.java
+++ src/main/java/woowacourse/chatting/domain/member/FriendRelation.java
@@ -2,27 +2,32 @@ package woowacourse.chatting.domain.member;
 
 import jakarta.persistence.*;
 import lombok.*;
+import woowacourse.chatting.domain.BaseEntity;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FriendRelation {
+public class FriendRelation extends BaseEntity {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "from_member")
     private Member from;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "to_member")
     private Member to;
 
     @Enumerated(EnumType.STRING)
     private FriendStatus status;
+
+    public void changeFriendStatus(FriendStatus status){
+        this.status = status;
+    }
 }
 
