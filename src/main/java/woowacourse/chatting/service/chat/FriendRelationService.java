26706ae517a304cfcfa52c540f34f6af606d33a4
oTAETAEo <ggg7515@naver.com>
2025-11-15 21:57:51 +0900

    feat(FriendRelationService) : 친구 요청이 발생하면 실행 될 비즈니스 기능 추가

--- src/main/java/woowacourse/chatting/service/chat/FriendRelationService.java
+++ src/main/java/woowacourse/chatting/service/chat/FriendRelationService.java
@@ -1,55 +1,68 @@
 package woowacourse.chatting.service.chat;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woowacourse.chatting.domain.member.FriendRelation;
+import woowacourse.chatting.domain.member.FriendStatus;
 import woowacourse.chatting.domain.member.Member;
+import woowacourse.chatting.dto.chat.FriendDto;
 import woowacourse.chatting.dto.chat.FriendResponseDto;
 import woowacourse.chatting.dto.chat.FriendsResponse;
 import woowacourse.chatting.repository.member.FriendRelationRepository;
-import woowacourse.chatting.service.MemberService;
 
 import java.util.List;
-import java.util.Set;
+import java.util.NoSuchElementException;
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class FriendRelationService {
 
-    private final MemberService memberService;
     private final FriendRelationRepository friendRelationRepository;
 
-    public void friendRequest(FriendRelation friendRelation){
-        friendRelationRepository.save(friendRelation);
+    public synchronized void friendRequest(FriendRelation friendRelation){
+        friendRelationRepository.findFriendRelationBetween(
+                friendRelation.getFrom().getId(), friendRelation.getTo().getId())
+                .ifPresentOrElse((f) -> {
+                    if (f.getStatus().equals(FriendStatus.REJECTED)){
+                        f.changeFriendStatus(FriendStatus.REQUESTED);
+                    }
+                }, () -> friendRelationRepository.save(friendRelation));
     }
 
     public FriendsResponse getAllFriendRequest(Member member){
 
-        Member findMember = memberService.findMember(member.getId());
-
-        Set<FriendRelation> sentFriendRequests = findMember.getSentFriendRequests();
-        Set<FriendRelation> receivedFriendRequests = findMember.getReceivedFriendRequests();
-
-        List<FriendResponseDto> sent = sentFriendRequests.stream()
-                .map(s -> FriendResponseDto.builder()
-                        .fromName(s.getFrom().getName())
-                        .toName(s.getTo().getName())
-                        .id(s.getId())
-                        .build())
-                .toList();
-
-        List<FriendResponseDto> received = receivedFriendRequests.stream()
-                .map(s -> FriendResponseDto.builder()
-                        .fromName(s.getFrom().getName())
-                        .toName(s.getTo().getName())
-                        .id(s.getId())
-                        .build())
-                .toList();
+        List<FriendResponseDto> sent = friendRelationRepository.findAllSentFriendRequests(member.getId());
+        List<FriendResponseDto> received = friendRelationRepository.findAllReceivedFriendRequests(member.getId());
+        List<FriendDto> friends = friendRelationRepository.findFriendList(member.getId());
         return FriendsResponse.builder()
                 .sentFriendRequests(sent)
                 .receivedFriendRequests(received)
+                .friends(friends)
                 .build();
+    }
+
+    public void friendStatusHandle(Member member, String status, Long id) {
+        FriendRelation friendRelation = friendRelationRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 친구추가 입니다."));
+
+        if(!friendRelation.getTo().getId().equals(member.getId())){
+            throw new IllegalArgumentException("권한 없음");
+        }
+
+        if (status.equalsIgnoreCase("ACCEPT")) {
+            friendRelation.changeFriendStatus(FriendStatus.ACCEPTED);
+            return;
+        }
+
+        if (status.equalsIgnoreCase("REJECT")) {
+            friendRelation.changeFriendStatus(FriendStatus.REJECTED);
+            return;
+        }
+
+        throw new IllegalArgumentException("잘못된 action");
     }
 }
