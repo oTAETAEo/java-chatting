932d187b4248d54e933691ffdb630f76c6e9b6b9
oTAETAEo <ggg7515@naver.com>
2025-11-15 21:52:16 +0900

    refactor(FriendsResponse) : 친구 정보를 보여주기 위한 list 타입 변경

--- src/main/java/woowacourse/chatting/dto/chat/FriendsResponse.java
+++ src/main/java/woowacourse/chatting/dto/chat/FriendsResponse.java
@@ -10,5 +10,5 @@ import java.util.List;
 public class FriendsResponse {
     private List<FriendResponseDto> sentFriendRequests;
     private List<FriendResponseDto> receivedFriendRequests;
-    private List<String> friends;
+    private List<FriendDto> friends;
 }
