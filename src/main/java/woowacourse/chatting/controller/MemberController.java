22e3bac2b17766fc16b34ad502a0162df3a1ff9e
oTAETAEo <ggg7515@naver.com>
2025-11-15 21:58:19 +0900

    feat(MemberController) : 친구 요청 엔드포인트 추가

--- src/main/java/woowacourse/chatting/controller/MemberController.java
+++ src/main/java/woowacourse/chatting/controller/MemberController.java
@@ -4,19 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import woowacourse.chatting.domain.member.FriendRelation;
 import woowacourse.chatting.domain.member.Member;
 import woowacourse.chatting.dto.AddFriendDto;
 import woowacourse.chatting.dto.ResponseDto;
-import woowacourse.chatting.dto.chat.FriendResponseDto;
+import woowacourse.chatting.dto.chat.FriendRequestStatusDto;
 import woowacourse.chatting.dto.chat.FriendsResponse;
 import woowacourse.chatting.service.MemberService;
 import woowacourse.chatting.service.chat.FriendRelationService;
-
-import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @RestController
@@ -33,11 +29,21 @@ public class MemberController {
         return ResponseEntity.ok().body(new ResponseDto("친구요청 완료."));
     }
 
-    @Transactional
     @GetMapping("/friends")
     public ResponseEntity<?> getFriendRequest(@AuthenticationPrincipal Member member){
         FriendsResponse allFriendRequest = friendRelationService.getAllFriendRequest(member);
         return ResponseEntity.ok()
                 .body(allFriendRequest);
+    }
+
+    @PatchMapping("/friend/status/{id}")
+    public ResponseEntity<?> friendRequestSuccess(
+            @RequestBody FriendRequestStatusDto statusDto,
+            @PathVariable Long id,
+            @AuthenticationPrincipal Member member) {
+
+        friendRelationService.friendStatusHandle(member, statusDto.getStatus(), id);
+
+        return ResponseEntity.ok().build();
     }
 }
