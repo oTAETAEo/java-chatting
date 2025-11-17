3ca1824f352cc32c92df940b3f8d6b030faee3ff
oTAETAEo <ggg7515@naver.com>
2025-11-17 13:41:47 +0900

    feat(UUIDUtil) : 외부에서 넘어오는 String 형식의 UUID 검증 및 반환 기능

---
+++ src/main/java/woowacourse/chatting/util/UUIDUtil.java
@@ -0,0 +1,16 @@
+package woowacourse.chatting.util;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    private UUIDUtil() {} // 인스턴스화 방지
+
+    public static UUID toUUID(String uuidString) {
+        try {
+            return UUID.fromString(uuidString);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 UUID 형식입니다: " + uuidString);
+        }
+    }
+}
