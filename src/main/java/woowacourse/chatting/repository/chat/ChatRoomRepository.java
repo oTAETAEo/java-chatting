ff32e6b98a1ef969707d07dbd6a7d5f9386d5866
oTAETAEo <ggg7515@naver.com>
2025-11-17 13:36:02 +0900

    refactor(ChatRoomRepository) : 중간 엔티티를 통한 채팅방 조회 쿼리 작성

--- src/main/java/woowacourse/chatting/repository/chat/ChatRoomRepository.java
+++ src/main/java/woowacourse/chatting/repository/chat/ChatRoomRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import woowacourse.chatting.domain.chat.ChatRoom;
 import woowacourse.chatting.domain.chat.ChatRoomType;
+import woowacourse.chatting.domain.member.Member;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,14 +14,14 @@ import java.util.UUID;
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, UUID> {
 
-
     @Query("""
-            select r from ChatRoom r
-                join r.members m
-                where m.email in (:userA, :userB) and r.roomType = :type
-                group by r
-                having count(distinct m) = 2
-            """)
-    Optional<ChatRoom> findByUsers(@Param("userA") String userA, @Param("userB") String userB, @Param("type") ChatRoomType type);
+        select r from ChatRoom r
+        join ChatRoomMember m1 on m1.chatRoom = r
+        join ChatRoomMember m2 on m2.chatRoom = r
+        where r.roomType = 'PRIVATE'
+          and m1.member = :member1
+          and m2.member = :member2
+    """)
+    Optional<ChatRoom> findPrivateRoomByMembers(Member member1, Member member2);
 
 }
