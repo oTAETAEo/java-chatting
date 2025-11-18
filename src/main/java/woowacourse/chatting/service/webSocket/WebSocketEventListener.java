2ac79be37f9c0f16e2e6a27f34290f87235be50f
oTAETAEo <ggg7515@naver.com>
2025-11-18 14:18:09 +0900

    feat(WebSocketEventListener) : 리스너를 통한 연결 메시지 전송
    -[추가] 연결, 연결을 끊는 경우 친구한테 온라인, 오프라인 메시지가 전송 되 실시간 접속 여부 확인 가능 합니다.

--- src/main/java/woowacourse/chatting/service/webSocket/WebSocketEventListener.java
+++ src/main/java/woowacourse/chatting/service/webSocket/WebSocketEventListener.java
@@ -8,15 +8,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import woowacourse.chatting.domain.member.Member;
+import woowacourse.chatting.dto.chat.ConnectFriendDto;
+import woowacourse.chatting.dto.chat.FriendDto;
+import woowacourse.chatting.repository.member.FriendRelationRepository;
 import woowacourse.chatting.service.MemberService;
 import woowacourse.chatting.util.UUIDUtil;
 
+import java.util.List;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
+    private final FriendRelationRepository friendRelationRepository;
     private final MemberService memberService;
     private final ConnectedUserService connectedUserService;
     private final SimpMessagingTemplate messagingTemplate;
@@ -29,13 +34,32 @@ public class WebSocketEventListener {
         UUID subId = UUIDUtil.toUUID(accessor.getUser().getName());
         Member member = memberService.findBySubId(subId);
         connectedUserService.addUser(sessionId, member);
+
+        List<FriendDto> friends = friendRelationRepository.findFriendList(member.getId());
+        for (FriendDto f: friends) {
+            messagingTemplate.convertAndSendToUser(
+                    f.getId().toString(),
+                    "/queue/friend/connect",
+                    new ConnectFriendDto(member.getSubId(), "online")
+            );
+        }
     }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
-
         String sessionId = accessor.getSessionId();
-        connectedUserService.removeUser(sessionId);
+        UUID subId = UUIDUtil.toUUID(accessor.getUser().getName());
+        Member member = memberService.findBySubId(subId);
+
+        List<FriendDto> friends = friendRelationRepository.findFriendList(member.getId());
+        for (FriendDto f: friends) {
+            messagingTemplate.convertAndSendToUser(
+                    f.getId().toString(),
+                    "/queue/friend/connect",
+                    new ConnectFriendDto(member.getSubId(), "offline")
+            );
+        }
+        connectedUserService.removeUser(sessionId, member);
     }
 }
