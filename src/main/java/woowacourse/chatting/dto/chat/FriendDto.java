79e4331a116c4338c586a84f79e79459e324efc6
oTAETAEo <ggg7515@naver.com>
2025-11-18 14:13:57 +0900

    refactor(FriendDto) : 접속 상태 필드 추가
    -[추가] PresenceStatus 필드를 추가해 친구 추가나, 최초 연결시 친구 연결 확인에 사용합니다.

--- src/main/java/woowacourse/chatting/dto/chat/FriendDto.java
+++ src/main/java/woowacourse/chatting/dto/chat/FriendDto.java
@@ -2,13 +2,24 @@ package woowacourse.chatting.dto.chat;
 
 import lombok.Builder;
 import lombok.Data;
+import woowacourse.chatting.domain.chat.PresenceStatus;
 
 import java.util.UUID;
 
 @Data
-@Builder
 public class FriendDto {
     private UUID id;      // 친구의 ID
     private String name;  // 친구 이름
     private String email; // 친구 email
+    private PresenceStatus status; // online - offline
+
+    @Builder
+    public FriendDto(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.status = PresenceStatus.OFFLINE;
+    }
+    public FriendDto() {
+    }
 }
