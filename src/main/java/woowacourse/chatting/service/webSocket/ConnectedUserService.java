2f311b7f88918d69e64853c7bff0210662a70d28
oTAETAEo <ggg7515@naver.com>
2025-11-18 14:15:58 +0900

    feat(ConnectedUserService) : 온라인 여부를 가지는 Set 추가

--- src/main/java/woowacourse/chatting/service/webSocket/ConnectedUserService.java
+++ src/main/java/woowacourse/chatting/service/webSocket/ConnectedUserService.java
@@ -1,10 +1,12 @@
 package woowacourse.chatting.service.webSocket;
 
 import org.springframework.stereotype.Service;
+import woowacourse.chatting.domain.chat.PresenceStatus;
 import woowacourse.chatting.domain.member.Member;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -12,23 +14,26 @@ public class ConnectedUserService {
 
     // sessionId -> Member 매핑
     private final Map<String, Member> connectedUsers = new ConcurrentHashMap<>();
+    private final Set<UUID> onlineUserIds = ConcurrentHashMap.newKeySet();
 
     public void addUser(String sessionId, Member member) {
         connectedUsers.put(sessionId, member);
+        onlineUserIds.add(member.getSubId());
     }
 
-    public void removeUser(String sessionId) {
+    public void removeUser(String sessionId, Member member) {
         connectedUsers.remove(sessionId);
+        onlineUserIds.remove(member.getSubId());
     }
 
     public Member getMember(String sessionId) {
         return connectedUsers.get(sessionId);
     }
 
-    public List<String> getConnectedUsernames() {
-        return connectedUsers.values()
-                .stream()
-                .map(Member::getName)
-                .toList();
+    public PresenceStatus containsFriend(UUID subId){
+        if(onlineUserIds.contains(subId)){
+            return PresenceStatus.ONLINE;
+        }
+        return PresenceStatus.OFFLINE;
     }
 }
